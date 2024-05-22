f88a4330a8875fead5fee4c027ac3e9daa6f5ed3
Limex7 <126993571+Limex7@users.noreply.github.com>
2024-05-21 19:14:50 -0700

    Update LowestCommonDemoninator.java
    fixed a major bug with not fully implementing the numerator

--- LowestCommonDemoninator.java
+++ LowestCommonDemoninator.java
@@ -1,12 +1,12 @@
 public class LowestCommonDemoninator {
     public static void main (String[] args){
-        System.out.println(lowestCommonDemoninator(3, 1, 6, 2));
+        System.out.println(lowestCommonDemoninator(1, 1, 6, 8));
     }
     public static int lowestCommonDemoninator(int num1, int num2, int denom1, int denom2, int i, int j){
-        if (denom1 % num1 == 0){
+        if (denom1 % num1 == 0 && num1 != 1){
             denom1 = (int) denom1/num1;
         }
-        if (denom2 % num1 == 0){
+        if (denom2 % num2 == 0 && num2 != 1){
             denom2 = (int) denom2/num2;
         }
         if (denom2 % denom1 == 0){
@@ -17,10 +17,10 @@ public class LowestCommonDemoninator {
         }
         else{
             if (denom1 < denom2){
-                return lowestCommonDemoninator(denom1 + denom1/i, denom2, i+1, j);
+                return lowestCommonDemoninator(num1, num2, denom1 + denom1/i, denom2, i+1, j);
             }
             else{
-                return lowestCommonDemoninator(denom1, denom2 + denom2/j, i, j+1);
+                return lowestCommonDemoninator(num1, num2, denom1, denom2 + denom2/j, i, j+1);
             }
         }
     }
@@ -29,3 +29,4 @@ public class LowestCommonDemoninator {
         return lowestCommonDemoninator(num1, num2, denom1, denom2, 1, 1);
     }
 }
+
